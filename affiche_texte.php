657d578f6b2e9e0ae8124a8cb94850719af596fb
Ténénan <tenenan.thebest@gmail.com>
2023-12-07 23:10:21 +0100

    Un test technique sur HTML et CSS

---
+++ affiche_texte.php
@@ -0,0 +1,45 @@
+<?php
+$texte = "Madame, Monsieur,
+
+Étudiant en conception et développement d’applications à l’ETNA, je suis ravi de vous présenter ma candidature pour le poste de développeur fullstack en alternance au sein de votre entreprise. Ayant une solide expérience dans le domaine de la programmation web, je suis convaincu que mes compétences et ma volonté de créer des nouvelles expériences pour les clients font de moi un candidat idéal pour rejoindre votre équipe professionnelle.
+
+Ayant à cœur de maitriser les dernières technologies et de découvrir d'autres langages et outils liés à la programmation, j’aime travailler en équipe comme de façon autonome. Très organisé, je suis capable de m'adapter à n'importe quelle situation dans le but de relever les défis imposés.
+
+Étudiant passionné par le développement et la programmation, je suis persuadé que  NEOVEE
+est l'endroit idéal pour mettre à profit mes compétences et mon enthousiasme. Je serais honoré de faire partie de votre équipe dynamique et de contribuer à la réputation d'excellence de votre établissement.
+
+Je vous remercie vivement de l'attention que vous voudrez bien porter à ma candidature et me tiens à votre disposition pour un entretien. 
+
+Dans l'attente de votre réponse, je vous prie d'agréer, Madame, Monsieur, l'expression de ma considération distinguée.
+
+Ténénan COULIBALY
+Étudiant en conception et développement d’applications à CESI, je suis ravi de vous présenter ma candidature pour le poste de développeur fullstack en alternance au sein de votre entreprise. Ayant une solide expérience dans le domaine de la programmation web, je suis convaincu que mes compétences et ma volonté de créer des nouvelles expériences pour les clients font de moi un candidat idéal pour rejoindre votre équipe professionnelle.
+
+Par ailleurs, devenir développeur fullstack est une volonté qui m'anime depuis la fin de ma première année de BTS. En effet, à l'issue de cette première année de BTS, j'ai effectué mon tout premier stage de développeur web à la Croix-Rouge française, où j'ai pu mettre en pratique mes acquis liés au développement web tout en apprenant de nouvelles technologies, telles que Ajax, jQuery, etc. Mon envie d'être ce développeur fullstack part donc de là. Cet été par exemple, en plus de me trouver une alternance, je développe mon portfolio en React afin de mettre en pratique mon autoformation lié à ce framework.
+
+Étudiant passionné par le développement et la programmation, je suis persuadé que le GROUPE BPCE est l'endroit idéal pour mettre à profit mes compétences et mon enthousiasme. Je serais honoré de faire partie de cette belle équipe dynamique tout en collaborant avec mon tuteur sur la réalisation des différentes taches et de contribuer à la réputation d'excellence de votre établissement.
+
+In fine, je vous remercie vivement de l'attention que vous voudrez bien porter à ma candidature et me tiens à votre disposition pour un entretien. 
+
+Dans l'attente de votre réponse, je vous prie d'agréer, Madame, Monsieur, l'expression de ma considération distinguée.
+
+Ténénan COULIBALY
+
+
+🔥 En recherche d'une entreprise d'accueil en développement et conception d'applications 🔥
+
+
+Cher/e réseau,
+
+
+
+";
+$mots = explode(" ", $texte);
+
+foreach ($mots as $mot) {
+    echo $mot . ' ';
+    flush(); // Pour vider le tampon de sortie immédiatement
+    sleep(56000); // Pause d'une seconde
+}
+
+?>
